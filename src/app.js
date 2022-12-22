d3e8f1b669f26be61e407840e5ab8d5e3ad0817d
Vladislav Shyrokiy <shyrokij009@gmail.com>
2022-12-22 17:15:04 +0200

    first-questions-site-firebase-js

---
+++ src/app.js
@@ -0,0 +1,58 @@
+import { Question } from './question'
+import { isValid, createModal } from './utilis'
+import './styles.css'
+import { getAuthForm, authWithEmailAndPassword } from './auth'
+
+const form = document.getElementById('form')
+const modalBtn = document.getElementById('modal-btn')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+window.addEventListener('load', Question.renderList)
+form.addEventListener('submit', submitFormHandler)
+modalBtn.addEventListener('click', openModal)
+input.addEventListener('input', () => {
+  submitBtn.disabled = !isValid(input.value)
+})
+function submitFormHandler(event) {
+  event.preventDefault()
+
+  if (isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON(),
+    }
+    submitBtn.disabled = true
+    Question.create(question)
+
+    input.value = ''
+    input.className = ''
+    submitBtn.disabled = false
+  }
+}
+function openModal() {
+  createModal('Authorization', getAuthForm())
+  document
+    .getElementById('auth-form')
+    .addEventListener('submit', authFromHandler, { once: true })
+}
+function authFromHandler(event) {
+  event.preventDefault()
+
+  const btn = event.target.querySelector('button')
+  const email = event.target.querySelector('#email').value
+  const password = event.target.querySelector('#password').value
+  btn.disabled = true
+
+  authWithEmailAndPassword(email, password)
+    .then(Question.fetch)
+    .then(renderModalAfterAuth)
+    .then(() => (btn.disabled = false))
+}
+function renderModalAfterAuth(content) {
+  if (typeof content === 'string') {
+    createModal('Error', content)
+  } else {
+    createModal('List Question', Question.listToHTML(content))
+  }
+}
---
+++ src/auth.js
@@ -0,0 +1,47 @@
+export function getAuthForm() {
+  return `
+    <form class="mui-form" id="auth-form">
+    <div class="mui-textfield mui-textfield--float-label">
+      <input
+        type="email"
+        id="email"
+        required
+      />
+      <label for="email">Email</label>
+    </div>
+    <div class="mui-textfield mui-textfield--float-label">
+    <input
+      type="password"
+      id="password"
+      required
+    />
+    <label for="password">Password</label>
+  </div>
+    <button
+      type="submit"
+      class="mui-btn mui-btn--raised mui-btn--primary"
+    >
+      Go it 
+    </button>
+  </form>
+    `
+}
+export function authWithEmailAndPassword(email, password) {
+  const API_KEY = 'AIzaSyBh65NObTsrOoE5OqnM97XnC56SDRkR_0k'
+  return fetch(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => data.idToken)
+}
